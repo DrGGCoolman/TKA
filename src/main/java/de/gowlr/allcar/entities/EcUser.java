d44d51a42d7d8685f1f05ac8cdca5fac8ed9046d
Jurij Befus <befus@ergonintern.de>
2019-09-02 10:55:12 +0200

    lombok

--- src/main/java/de/gowlr/allcar/entities/EcUser.java
+++ src/main/java/de/gowlr/allcar/entities/EcUser.java
@@ -1,68 +1,31 @@
 package de.gowlr.allcar.entities;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+import lombok.Getter;
+import lombok.Setter;
 
 public class EcUser {
 
+  @Id
+  @GeneratedValue
+  @Getter
   private long userId;
+  @Getter
+  @Setter
   private String name;
+  @Getter
+  @Setter
   private String firstname;
+  @Getter
+  @Setter
   private String gender;
+  @Getter
+  @Setter
   private String eMail;
+  @Getter
+  @Setter
   private String password;
-
-
-  public long getUserId() {
-    return userId;
-  }
-
-  public void setUserId(long userId) {
-    this.userId = userId;
-  }
-
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-
-  public String getFirstname() {
-    return firstname;
-  }
-
-  public void setFirstname(String firstname) {
-    this.firstname = firstname;
-  }
-
-
-  public String getGender() {
-    return gender;
-  }
-
-  public void setGender(String gender) {
-    this.gender = gender;
-  }
-
-
-  public String getEMail() {
-    return eMail;
-  }
-
-  public void setEMail(String eMail) {
-    this.eMail = eMail;
-  }
-
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
 
 }
