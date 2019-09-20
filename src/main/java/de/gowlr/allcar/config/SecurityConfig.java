3d94ce4e9ded1d7a867f46cb577a2fbff80951a7
Jurij Befus <jurij.befus@gmail.com>
2019-09-20 16:15:47 +0200

    sec conf

--- src/main/java/de/gowlr/allcar/config/SecurityConfig.java
+++ src/main/java/de/gowlr/allcar/config/SecurityConfig.java
@@ -17,14 +17,13 @@ import de.gowlr.allcar.web.CarFilterModel;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    // @Override
-    // protected void configure(HttpSecurity http) throws Exception {
-    // http.csrf().disable().authorizeRequests().antMatchers("/css/**", "/index",
-    // "/home", "/webjars/**").permitAll()
-    // .antMatchers("/user/**").hasRole("USER").antMatchers("/admin/**").hasRole("ADMIN").and().formLogin()
-    // .loginPage("/users/login").permitAll().and().logout().permitAll();
-    // // TODO: default errror failureUrl("/login-error").
-    // }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers("/css/**", "/index", "/home", "/webjars/**").permitAll()
+                .antMatchers("/user/**").hasRole("USER").antMatchers("/admin/**").hasRole("ADMIN").and().formLogin()
+                .failureUrl("/login-error").loginPage("/users/login").permitAll().and().logout().permitAll();
+        // TODO: default errror failureUrl("/login-error").
+    }
 
     @Autowired
     UserAdapterService userDetailsService;
@@ -41,40 +40,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // Setting Service to find User in the database.
         // And Setting PassswordEncoder
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http.csrf().disable();
-
-        // The pages does not require login
-        http.authorizeRequests().antMatchers("/", "/login", "/logout").permitAll();
-
-        // /userInfo page requires login as ROLE_USER or ROLE_ADMIN.
-        // If no login, it will redirect to /login page.
-        http.authorizeRequests().antMatchers("/userInfo").access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')");
-
-        // For ADMIN only.
-        http.authorizeRequests().antMatchers("/admin").access("hasRole('ROLE_ADMIN')");
-
-        // When the user has logged in as XX.
-        // But access a page that requires role YY,
-        // AccessDeniedException will be thrown.
-        http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
-
-        // Config for Login Form
-        http.authorizeRequests().and().formLogin()//
-                // Submit URL of login page.
-                .loginProcessingUrl("/j_spring_security_check") // Submit URL
-                .loginPage("/login")//
-                .defaultSuccessUrl("/userAccountInfo")//
-                .failureUrl("/login?error=true")//
-                .usernameParameter("username")//
-                .passwordParameter("password")
-                // Config for Logout Page
-                .and().logout().logoutUrl("/logout").logoutSuccessUrl("/logoutSuccessful");
 
     }
 
