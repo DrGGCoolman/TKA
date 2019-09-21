e5f01e1b0f157b020143de95dfa1068ecd972013
Jurij Befus <jurij.befus@gmail.com>
2019-09-22 01:47:24 +0200

    filter aus db

--- src/main/java/de/gowlr/allcar/repositories/ProductTypeRepository.java
+++ src/main/java/de/gowlr/allcar/repositories/ProductTypeRepository.java
@@ -4,6 +4,7 @@ package de.gowlr.allcar.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import de.gowlr.allcar.entities.EcProductTypeEntity;;
 
@@ -18,6 +19,27 @@ public interface ProductTypeRepository extends JpaRepository<EcProductTypeEntity
 
         List<EcProductTypeEntity> findByEcBrandByBrandIdBrandTitleContainingIgnoreCase(String brandTitle);
 
-        List<EcProductTypeEntity> findByHighlighted(Boolean highlighted);;
+        List<EcProductTypeEntity> findByHighlighted(Boolean highlighted);
+
+        @Query("SELECT DISTINCT fuelType FROM EcProductTypeEntity")
+        List<String> findDistinctFuels();
+
+        @Query("SELECT DISTINCT gearingType FROM EcProductTypeEntity")
+        List<String> findDistinctTransmissions();
+
+        @Query("SELECT DISTINCT driveSystem FROM EcProductTypeEntity")
+        List<String> findDistinctDirveSystems();
+
+        @Query("SELECT DISTINCT minimumAge FROM EcProductTypeEntity")
+        List<Integer> findDistinctDriverAges();
+
+        @Query("SELECT MIN(age) FROM EcProductTypeEntity")
+        Integer findMinYear();
+
+        @Query("SELECT MIN(powerPs) FROM EcProductTypeEntity")
+        Integer findMinPower();
+
+        @Query("SELECT MAX(age) FROM EcProductTypeEntity")
+        Integer findMaxYear();
 
 }
